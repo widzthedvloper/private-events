874675dd5cf6f32d0ec0616e7f6ca5087c0d04ee
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-24 15:53:23 -0400

    Add route to the index to show all the events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+lass UsersController < ApplicationController
   before_action :set_user, only: %i[show]
 
   # GET /users/1 or /users/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/login', to: 'users#login'
   post '/login', to: 'users#signin'
 
-  resources :events, only: [:new, :create, :show]
+  resources :events, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
