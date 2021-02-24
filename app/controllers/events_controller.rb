75de588d0b0c8c5d42b1b1b262cc50168159d2f3
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-24 14:07:27 -0400

    Create routes for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,13 +14,10 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
-  # GET /events/1/edit
-  def edit; end
-
   # POST /events or /events.json
   def create
     @event = Event.new(event_params)
-
+    @event.creator = session[:user_id]
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
@@ -29,28 +26,6 @@ class EventsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /events/1 or /events/1.json
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /events/1 or /events/1.json
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -63,6 +38,6 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.require(:event).permit(:name, :description, :date, :creator)
+    params.require(:event).permit(:name, :description, :date)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
     validates :name, presence: true, length: {minimum:3, maximum:30}
     validates :description, presence: true
-
+    validates :creator, presence: true
     belongs_to :user, class_name: "User", foreign_key: "creator"
     has_many :invitations, foreign_key: "attended_event"
     has_many :atendee, through: "invitations"
