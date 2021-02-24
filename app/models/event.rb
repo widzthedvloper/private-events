243121868389c4d8063dfb3f549fa03d5c5ea6c5
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-24 13:44:21 -0400

    Validate and add association for event table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+    validates :name, presence: true, length: {minimum:3, maximum:30}
+    validates :description, presence: true
+
+    belongs_to :user, class_name: "User", foreign_key: "creator"
+    has_many :invitations, foreign_key: "attended_event"
+    has_many :atendee, through: "invitations"
 end
