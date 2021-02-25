b54254d510f05cfda75fcb9f289a292ed8282dc8
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-25 15:38:16 -0400

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   get '/login', to: 'users#login'
   post '/login', to: 'users#signin'
-  root to: 'users#login'
+
+  root to: "events#index"
   resources :events, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
