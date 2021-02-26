a48bb907fa668cba25aa43747030f46227e4bffa
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-26 12:57:44 -0400

    Create test for the event  model validations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:event) { FactoryBot.build(:event) }
+    it 'should have a name' do
+      event.name = nil
+      expect(event).to_not be_valid
+    end
+
+    it 'should have a description' do
+      event.description = nil
+      expect(event).to_not be_valid
+    end
+
+    it 'should have a creator' do
+      event.creator = nil
+      expect(event).to_not be_valid
+    end
+  end
 end
