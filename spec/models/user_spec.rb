9aeef17437c64ad3430f473e6d873166a74a93de
Oever González <software@notengobattery.com>
2021-02-26 10:35:00 -0600

    implement a basic email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:user) { FactoryBot.build(:user) }
+    it 'should have an email' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+  end
 end
