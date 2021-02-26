1827548c47bedf2d942fbf99e7805dd49a009950
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-02-26 12:51:06 -0400

    Create test for invitation model validations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,6 @@
 class Invitation < ApplicationRecord
+  validates :atendee, presence: true
+  validates :attended_event, presence: true
   belongs_to :atendee, class_name: 'User', foreign_key: 'atendee'
   belongs_to :attended_event, class_name: 'Event', foreign_key: 'attended_event'
 end
--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:invitation) { FactoryBot.build(:user).invitations.build }
+    it 'should have an attendee' do
+      invitation.atendee = nil
+      expect(invitation).to_not be_valid
+    end
+
+    it 'should have an attended_event' do
+      invitation.attended_event = nil
+      expect(invitation).to_not be_valid
+    end
+  end
 end
